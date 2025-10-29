37b538e897f9f3aa9ff692cd00b69883d64caabe
egvimo <egvimo@gmail.com>
2025-10-29 14:30:58 +0100

    test: improve chart testing

--- ci/prepare-cnpg.sh
+++ ci/prepare-cnpg.sh
@@ -3,5 +3,4 @@ set -euo pipefail
 trap 'echo "❌ Error: Command \"${BASH_COMMAND}\" failed at line $LINENO"' ERR
 set -x
 
-helm repo add cnpg https://cloudnative-pg.github.io/charts/
 kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/refs/heads/main/example/prometheus-operator-crd/monitoring.coreos.com_podmonitors.yaml
