4a49ee9c32fe93abf801733f963b9a0399191172
egvimo <egvimo@gmail.com>
2026-02-11 20:35:38 +0100

    feat(authelia): add access control

--- charts/authelia/ci/prepare.sh
+++ charts/authelia/ci/prepare.sh
@@ -2,9 +2,5 @@
 set -euo pipefail
 trap 'echo "❌ Error: Command \"${BASH_COMMAND}\" failed at line $LINENO"' ERR
 
-CERT_MANAGER_VERSION=$(yq '.dependencies[] | select(.name == "cert-manager") | .version' charts/cert-manager/Chart.yaml)
-
-kubectl apply -f "https://github.com/cert-manager/cert-manager/releases/download/$CERT_MANAGER_VERSION/cert-manager.crds.yaml"
-
 # TODO Use latest version
 kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v3.6/docs/content/reference/dynamic-configuration/kubernetes-crd-definition-v1.yml
