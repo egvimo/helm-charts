9909df7ec65b613941de9ed065335e0964413197
egvimo <egvimo@gmail.com>
2026-05-01 20:49:12 +0200

    feat(blocky): add monitoring

---
+++ charts/blocky/ci/prepare.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -euo pipefail
+trap 'echo "❌ Error: Command \"${BASH_COMMAND}\" failed at line $LINENO"' ERR
+
+kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/refs/heads/main/example/prometheus-operator-crd/monitoring.coreos.com_servicemonitors.yaml
